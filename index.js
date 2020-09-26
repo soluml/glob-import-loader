cce378e19e3c8c7cbb2675ce74a11036629b9637
soluml <benjamin@soluml.com>
2020-09-25 20:57:17 -0500

    Still testing

--- index.js
+++ index.js
@@ -1,55 +1,96 @@
 const path = require("path");
-const fs = require("fs");
 const glob = require("glob");
 const replaceAsync = require("string-replace-async");
 const loaderUtils = require("loader-utils");
 const enhancedResolver = require("enhanced-resolve");
+const { rejects } = require("assert");
 
 module.exports = async function (source) {
   this.cacheable && this.cacheable(true);
 
+  const callback = this.async();
   const regex = /.?import + ?((\w+) +from )?([\'\"])(.*?);?\3/gm;
   const importModules = /import +(\w+) +from +([\'\"])(.*?)\2/gm;
   const importFiles = /import +([\'\"])(.*?)\1/gm;
   const importSass = /@import +([\'\"])(.*?)\1/gm;
+  const options = Object.assign({}, loaderUtils.getOptions(this));
+  const resolvePath = (pathToResolve) => {
+    return new Promise((resolve, reject) => {
+      enhancedResolver.create(options.resolve || {})(
+        this,
+        path.dirname(this.resourcePath),
+        pathToResolve,
+        (err, result) => {
+          if (err && !result) {
+            console.log("MY MISSING", err.missing);
 
-  source.replace(regex, (match, fromStatement, obj, quote, filename) => {
-    console.log("1", match);
-    console.log("2", fromStatement);
-    console.log("3", obj);
-    console.log("4", quote);
-    console.log("5", filename);
-  });
+            if (Array.isArray(err.missing)) {
+              // Use the path without extensions
+              resolve(err.missing.sort()[0]);
+            } else {
+              callback(new Error("Could not resolve the wildcard path."));
+            }
+
+            resolve(result);
+          }
+        }
+      );
+    });
+  };
 
   await replaceAsync(
     source,
     regex,
-    (match, fromStatement, obj, quote, filename) => {
-      console.log("A1", match);
-      console.log("A2", fromStatement);
-      console.log("A3", obj);
-      console.log("A4", quote);
-      console.log("A5", filename);
+    async (match, fromStatement, obj, quote, filename) => {
+      // If there are no wildcards, return early
+      if (!filename.match(/\*/)) {
+        return match;
+      }
+
+      const modules = [];
+      const withModules = false;
+      const globRelativePath = filename.match(/!?([^!]*)$/)[1];
+      const prefix = filename.replace(globRelativePath, "");
+
+      const temp = await resolvePath(globRelativePath);
+
+      console.log("NAME", globRelativePath, temp);
+
+      // var result = glob
+      //   .sync(globRelativePath, {
+      //     cwd: cwdPath,
+      //   })
+      //   .map((file, index) => {
+      //     var fileName = quote + prefix + file + quote;
+
+      //     if (match.match(importSass)) {
+      //       return "@import " + fileName;
+      //     } else if (match.match(importModules)) {
+      //       var moduleName = obj + index;
+      //       modules.push(moduleName);
+      //       withModules = true;
+      //       return "import * as " + moduleName + " from " + fileName;
+      //     } else if (match.match(importFiles)) {
+      //       return "import " + fileName;
+      //     } else {
+      //       self.emitWarning('Unknown import: "' + match + '"');
+      //     }
+      //   })
+      //   .join("; ");
+
+      // if (result && withModules) {
+      //   result += "; var " + obj + " = [" + modules.join(", ") + "]";
+      // }
+
+      // if (!result) {
+      //   self.emitWarning('Empty results for "' + match + '"');
+      // }
+
+      // return result;
     }
   );
 
-  console.log(source);
+  // console.log(source);
 
-  this.callback(null, source);
+  callback(null, source);
 };
-
-//   function resolvePath(pathToResolve) {
-//     return new Promise((resolve, reject) => {
-//       myEnhancedResolver(self, path.dirname(self.resourcePath), pathToResolve, (err, result) => {
-//         if (err && !result) {
-//           if(Array.isArray(err.missing)) {
-//             result = err.missing.sort()[0];
-//           } else {
-//             reject(err);
-//           }
-//         }
-
-//         resolve(result);
-//       });
-//     });
-//   }
